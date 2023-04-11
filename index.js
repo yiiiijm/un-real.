cae765cdca900e70d92274452474ee2710459a26
yiiiijm <130348686+yiiiijm@users.noreply.github.com>
2023-04-11 11:36:21 +0900

    Update index.js

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ const dataArr = [
         "link": "https://gokweol11.yikyung.repl.co",
     },
     {
-     //   "image": "https://i.postimg.cc/gj2256z7/1.jpg",
+     //   "image": "https://png.pngtree.com/png-clipart/20200704/ourmid/pngtree-black-gradient-radial-gradient-png-image_2274808.jpg",
         "link": "https://gokweol10.yikyung.repl.co",
     },
      {
@@ -95,7 +95,7 @@ const dataArr = [
         "link": "https://gokweol7.yikyung.repl.co",
     },
     {
-        "image": "https://i.postimg.cc/9fQdy2nt/Discord-A-New-Way-to-Chat-with-Friends-Communities.jpg",
+        "image": "https://png.pngtree.com/png-clipart/20200704/ourmid/pngtree-black-gradient-radial-gradient-png-image_2274808.jpg",
         "link": "https://gokweol6.yikyung.repl.co",
     },
     {
@@ -151,11 +151,11 @@ const dataArr = [
         "link": "https://test2.yikyung.repl.co",
     },
     {
-        "image": "https://i.postimg.cc/ZqSD48GC/image.jpg",
+        "image": "https://png.pngtree.com/png-clipart/20200704/ourmid/pngtree-black-gradient-radial-gradient-png-image_2274808.jpg",
         "link": "https://gokweol13.yikyung.repl.co",
     },
     {
-        "image": "https://i.postimg.cc/prWmH6Zq/image.jpg",
+        "image": "https://png.pngtree.com/png-clipart/20200704/ourmid/pngtree-black-gradient-radial-gradient-png-image_2274808.jpg",
         "link": "https://gokweol12.yikyung.repl.co",
     },
     {
@@ -163,7 +163,7 @@ const dataArr = [
         "link": "https://gokweol11.yikyung.repl.co",
     },
     {
-      //  "image": "https://i.postimg.cc/gj2256z7/1.jpg",
+      //  "image": "https://png.pngtree.com/png-clipart/20200704/ourmid/pngtree-black-gradient-radial-gradient-png-image_2274808.jpg",
         "link": "https://gokweol10.yikyung.repl.co",
     },
     {
@@ -323,7 +323,7 @@ const dataArr = [
         "link": "https://gokweol3.yikyung.repl.co",
     },
     {
-        "image": "https://i.postimg.cc/YSV13MtY/6.jpg",
+        "image": "https://png.pngtree.com/png-clipart/20200704/ourmid/pngtree-black-gradient-radial-gradient-png-image_2274808.jpg",
         "link": "https://gokweol3.yikyung.repl.co",
     },
     {
@@ -343,7 +343,7 @@ const dataArr = [
         "link": "https://test4.yikyung.repl.co",
     },
     {
-        "image": "https://i.postimg.cc/hGnr7JHJ/1.jpg",
+        "image": "https://png.pngtree.com/png-clipart/20200704/ourmid/pngtree-black-gradient-radial-gradient-png-image_2274808.jpg",
         "link": "https://test3.yikyung.repl.co",
     },
     {
